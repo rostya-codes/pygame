35d819f798d21c2216ef51021affa891646e311c
Rostya <rostyamudrik@proton.me>
2024-03-04 23:12:41 +0200

    Sprite, background, animation, music added

--- main.py
+++ main.py
@@ -1,29 +1,54 @@
 import pygame
 
+clock = pygame.time.Clock()
+
 pygame.init()  # Дальше будет происходить разработка игры
 
 # Задаем размер окна
-screen = pygame.display.set_mode((600, 300))  # С помощью <flags=pygame.NOFRAME> можно скрыть верхние кнопки
+screen = pygame.display.set_mode((700, 400))  # С помощью <flags=pygame.NOFRAME> можно скрыть верхние кнопки
 
 pygame.display.set_caption('Rostya pygame!')  # Название для окна
 icon = pygame.image.load('images/icon.png')  # Сохранить иконку
 pygame.display.set_icon(icon)  # Указать иконку
 
-square = pygame.Surface((50, 200))  # Поверхность
-square.fill('Blue')  # Указать цвет заливки
+bg = pygame.image.load('images/bg.jpg')
+walk_left = [  # To move to the left side of screen
+    pygame.image.load('images/player_left/0.png'),
+    pygame.image.load('images/player_left/1.png'),
+    pygame.image.load('images/player_left/2.png'),
+    pygame.image.load('images/player_left/3.png'),
+]
 
-myfont = pygame.font.Font('fonts/Micro_5/Micro5-Regular.ttf', 40)
-text_surface = myfont.render('Rostya', True, 'Red')
+walk_right = [  # To move to the right side of screen
+    pygame.image.load('images/player_right/0.png'),
+    pygame.image.load('images/player_right/1.png'),
+    pygame.image.load('images/player_right/2.png'),
+    pygame.image.load('images/player_right/3.png'),
+]
 
-player = pygame.image.load('images/icon.png')
+player_anim_count = 0
+bg_x = 0
+
+bg_sound = pygame.mixer.Sound('sounds/bg.mp3')
+# Установка начальной громкости фоновой музыки
+bg_sound.set_volume(0.4)  # Начальная громкость 40%
+bg_sound.play()
 
 running = True
 while running:
 
-    pygame.draw.circle(square, 'Red', (35, 185), 15)  # draw - нарисовать
-    screen.blit(square, (20, 50))  # В кортеже передаются координаты(x, y)
-    screen.blit(text_surface, (300, 100))
-    screen.blit(player, (100, 50))
+    screen.blit(bg, (bg_x, 0))
+    screen.blit(bg, (bg_x + 700, 0))
+    screen.blit(walk_right[player_anim_count], (300, 315))
+
+    if player_anim_count == 3:
+        player_anim_count = 0
+    else:
+        player_anim_count += 1
+
+    bg_x -= 5
+    if bg_x == -700:
+        bg_x = 0
 
     pygame.display.update()
 
@@ -32,3 +57,5 @@ while running:
             # После отключения никакие дополнительные действия не должны выполняться
             running = False
             pygame.quit()
+
+    clock.tick(15)  # Указываем количество кадров в секунду
