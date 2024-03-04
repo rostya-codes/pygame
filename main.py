467c7802309617cb89384c30f7c7b4a90ad58eaf
Rostya <rostyamudrik@proton.me>
2024-03-04 19:55:49 +0200

    Font and other changes added

--- main.py
+++ main.py
@@ -9,10 +9,21 @@ pygame.display.set_caption('Rostya pygame!')  # Название для окна
 icon = pygame.image.load('images/icon.png')  # Сохранить иконку
 pygame.display.set_icon(icon)  # Указать иконку
 
+square = pygame.Surface((50, 200))  # Поверхность
+square.fill('Blue')  # Указать цвет заливки
+
+myfont = pygame.font.Font('fonts/Micro_5/Micro5-Regular.ttf', 40)
+text_surface = myfont.render('Rostya', True, 'Red')
+
+player = pygame.image.load('images/icon.png')
+
 running = True
 while running:
 
-    #screen.fill((33, 26, 58))
+    pygame.draw.circle(square, 'Red', (35, 185), 15)  # draw - нарисовать
+    screen.blit(square, (20, 50))  # В кортеже передаются координаты(x, y)
+    screen.blit(text_surface, (300, 100))
+    screen.blit(player, (100, 50))
 
     pygame.display.update()
 
@@ -21,6 +32,3 @@ while running:
             # После отключения никакие дополнительные действия не должны выполняться
             running = False
             pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:  # Проверка на нажатие клавишы
-                screen.fill((33, 26, 100))  # Изменение цвета экрана при нажатии на клавишу
